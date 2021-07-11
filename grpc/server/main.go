2b48ff8064186b39347d92c1f5b1b7055e63d63f
Mike Castner <mjcastner@gmail.com>
2021-07-11 02:48:12 -0700

    Refactor gRPC DB connection / initialization code into not total garbage

--- grpc/server/main.go
+++ grpc/server/main.go
@@ -23,80 +23,84 @@ type server struct {
 	server_proto.UnimplementedGoHomeServer
 }
 
-func (s *server) BatchGet(ctx context.Context, in *server_proto.LinkRequestBatch) (*server_proto.LinkBatch, error) {
-	log.Printf("Received Batch Get request: %s", in)
-	var (
-		name       string
-		target_url string
-		views      int64
-	)
-
-	links := &server_proto.LinkBatch{}
-	rows, err := DB.Query("SELECT name, target_url, views FROM links LIMIT 500;")
-	if err != nil {
-		log.Println(err)
-	}
-
-	for rows.Next() {
-		rows.Scan(&name, &target_url, &views)
-		link := &link_proto.Link{
-			Name:      name,
-			TargetUrl: target_url,
-			Views:     views,
-		}
-		links.Links = append(links.Links, link)
-	}
-
-	return links, nil
+type connection struct {
+	name       string
+	host       string
+	port       string
+	driver     string
+	socket     bool
+	socket_dir string
+	user       string
+	password   string
 }
 
-func (s *server) Get(ctx context.Context, in *server_proto.LinkRequest) (*link_proto.Link, error) {
-	var (
-		name       string
-		target_url string
-		views      int64
-	)
-
-	log.Printf("Received Get request: %s", in)
-	err := DB.QueryRow("SELECT name, target_url, views FROM links WHERE name=$1", in.Name).Scan(&name, &target_url, &views)
-	if err != nil {
-		log.Println(err)
+// DB methods
+func getDriver(dbType string) string {
+	var driver string
+	switch dbType {
+	case "postgres":
+		driver = "pgx"
+	case "sqlite":
+		driver = "sqlite"
+	default:
+		log.Fatalf("Unsupported database: %s", dbType)
 	}
-
-	link := &link_proto.Link{
-		Name:      name,
-		TargetUrl: target_url,
-		Views:     views,
-	}
-	return link, nil
+	return driver
 }
 
-func (s *server) Set(ctx context.Context, in *link_proto.Link) (*server_proto.SetResponse, error) {
-	log.Printf("Received Set request for key %s: %s", in.Name, in.TargetUrl)
+func initConn(connection connection, safeConn bool) {
+	log.Println("Initializing connection to database...")
+	var dbUri string
 
-	_, err := DB.Exec(
-		`INSERT INTO
-			 links (name, target_url, views)
-		 VALUES ($1, $2, $3)
-		 ON CONFLICT (name)
-		 DO UPDATE 
-			 SET target_url = $2, views = $3;`,
-		in.Name,
-		in.TargetUrl,
-		in.Views,
-	)
-	if err != nil {
-		log.Println(err)
-		response := &server_proto.SetResponse{
-			Code: 1,
+	if safeConn {
+		switch connection.driver {
+		case "sqlite":
+			dbUri = fmt.Sprintf("/tmp/%s.db", connection.name)
+		case "pgx":
+			dbUri = fmt.Sprintf(
+				"postgres://%s:%s@%s:%s/",
+				connection.user,
+				connection.password,
+				connection.host,
+				connection.port)
 		}
-		return response, err
+		if connection.socket {
+			dbUri = fmt.Sprintf(
+				"%s:%s@unix(/%s/%s)/?parseTime=true",
+				connection.user,
+				connection.password,
+				connection.socket_dir,
+				connection.host)
+		}
 	} else {
-		response := &server_proto.SetResponse{
-			Code: 0,
+		switch connection.driver {
+		case "sqlite":
+			dbUri = fmt.Sprintf("/tmp/%s.db%s", connection.name, connection.name)
+		case "pgx":
+			dbUri = fmt.Sprintf(
+				"postgres://%s:%s@%s:%s/%s",
+				connection.user,
+				connection.password,
+				connection.host,
+				connection.port,
+				connection.name)
 		}
-		return response, nil
+		if connection.socket {
+			dbUri = fmt.Sprintf(
+				"%s:%s@unix(/%s/%s)/%s?parseTime=true",
+				connection.user,
+				connection.password,
+				connection.socket_dir,
+				connection.host,
+				connection.name)
+		}
 	}
+	log.Println(dbUri)
+	db, err := sql.Open(connection.driver, dbUri)
+	if err != nil {
+		log.Fatal(err)
+	}
+	DB = db
 }
 
 func initDb(ctx context.Context, dbName string) bool {
@@ -131,66 +135,123 @@ func initTable() {
 	}
 }
 
+// Batch methods
+func (s *server) BatchGet(ctx context.Context, in *server_proto.LinkRequestBatch) (*server_proto.LinkBatch, error) {
+	log.Printf("Received Batch Get request: %s", in)
+	var (
+		name       string
+		target_url string
+		views      int64
+	)
+
+	links := &server_proto.LinkBatch{}
+	rows, err := DB.Query("SELECT name, target_url, views FROM links LIMIT 500;")
+	if err != nil {
+		log.Println(err)
+	}
+
+	for rows.Next() {
+		rows.Scan(&name, &target_url, &views)
+		link := &link_proto.Link{
+			Name:      name,
+			TargetUrl: target_url,
+			Views:     views,
+		}
+		links.Links = append(links.Links, link)
+	}
+
+	return links, nil
+}
+
+// Standard methods
+func (s *server) Get(ctx context.Context, in *server_proto.LinkRequest) (*link_proto.Link, error) {
+	var (
+		name       string
+		target_url string
+		views      int64
+	)
+
+	log.Printf("Received Get request: %s", in)
+	err := DB.QueryRow(
+		"SELECT name, target_url, views FROM links WHERE name=$1",
+		in.Name).Scan(&name, &target_url, &views)
+	if err != nil {
+		log.Println(err)
+	}
+
+	link := &link_proto.Link{
+		Name:      name,
+		TargetUrl: target_url,
+		Views:     views,
+	}
+	return link, nil
+}
+
+func (s *server) Set(ctx context.Context, in *link_proto.Link) (*server_proto.SetResponse, error) {
+	log.Printf("Received Set request for key %s: %s", in.Name, in.TargetUrl)
+
+	_, err := DB.Exec(
+		`INSERT INTO
+			 links (name, target_url, views)
+		 VALUES ($1, $2, $3)
+		 ON CONFLICT (name)
+		 DO UPDATE SET target_url = $2, views = $3;`,
+		in.Name,
+		in.TargetUrl,
+		in.Views,
+	)
+	if err != nil {
+		log.Println(err)
+		response := &server_proto.SetResponse{
+			Code: 1,
+		}
+		return response, err
+	} else {
+		response := &server_proto.SetResponse{
+			Code: 0,
+		}
+		return response, nil
+	}
+}
+
 func main() {
 	dbNameFlag := flag.String("db_name", "gohome", "Database name.")
-	dbTypeFlag := flag.String("db_type", "sqlite", "Database type (sqlite and postgres are supported).")
 	dbHostFlag := flag.String("db_host", "", "Database host name.")
-	dbUserFlag := flag.String("user", "", "User for DB access.")
-	dbPasswordFlag := flag.String("password", "", "Password for DB access.")
+	dbPortFlag := flag.String("db_port", "5432", "Port for database connection.")
+	dbUserFlag := flag.String("db_user", "", "User for DB access.")
+	dbPasswordFlag := flag.String("db_password", "", "Password for DB access.")
+	dbSocketDirFlag := flag.String("socket_dir", "", "UNIX socket name.")
+	dbTypeFlag := flag.String(
+		"db_type",
+		"sqlite",
+		"Database type (sqlite and postgres are supported).")
+	dbSocketFlag := flag.Bool(
+		"use_socket",
+		false,
+		"Whether or not to use UNIX sockets for DB connections.")
 	flag.Parse()
 
-	// dbURI = fmt.Sprintf("%s:%s@unix(/%s/%s)/%s?parseTime=true", dbUser, dbPwd, socketDir, instanceConnectionName, dbName)
-	// Look for Cloud SQL instance connection variable
-	dbHostEnvVar := os.Getenv("GOHOME_DB_HOST")
-	if *dbHostFlag == "" && dbHostEnvVar != "" {
-		log.Printf("Found Cloud SQL instance name: %s", dbHostEnvVar)
-		dbHostFlag = &dbHostEnvVar
+	// Init connection
+	connection := connection{
+		name:       *dbNameFlag,
+		host:       *dbHostFlag,
+		port:       *dbPortFlag,
+		driver:     getDriver(*dbTypeFlag),
+		socket:     *dbSocketFlag,
+		socket_dir: *dbSocketDirFlag,
+		user:       *dbUserFlag,
+		password:   *dbPasswordFlag,
 	}
 
-	dbPasswordEnvVar := os.Getenv("GOHOME_DB_PASSWORD")
-	if *dbPasswordFlag == "" && dbPasswordEnvVar != "" {
-		dbPasswordFlag = &dbPasswordEnvVar
-	}
-
-	// Init DB connection
+	// Perform initial setup, if necessary
 	ctx := context.Background()
-	var dbDriver string
-	var dbHostUrl string
-	if *dbTypeFlag == "sqlite" {
-		log.Println("Initializing connection to SQLite DB...")
-		dbDriver = "sqlite"
-		dbHostUrl = fmt.Sprintf("/tmp/%s.db", *dbNameFlag)
-	} else if *dbTypeFlag == "postgres" {
-		log.Println("Initializing connection to Postgres DB...")
-		dbDriver = "pgx"
-		dbHostUrl = fmt.Sprintf("postgres://%s:%s@%s:5432/", *dbUserFlag, *dbPasswordFlag, *dbHostFlag)
-	}
-	log.Printf("Connecting to %s DB at %s...", *dbTypeFlag, dbHostUrl)
-	db, err := sql.Open(dbDriver, dbHostUrl)
-	if err != nil {
-		log.Fatal(err)
-	}
-	DB = db
-	defer db.Close()
-	defer DB.Close()
-
-	// Init root DB / table
-	if *dbTypeFlag != "sqlite" {
-		db.Ping()
-		dbReady := initDb(ctx, *dbNameFlag)
-		if dbReady {
-			initializedUrl := fmt.Sprintf("%s%s", dbHostUrl, *dbNameFlag)
-			log.Println(initializedUrl)
-			db, err := sql.Open(dbDriver, initializedUrl)
-			if err != nil {
-				log.Fatal(err)
-			}
-			DB = db
-		} else {
-			log.Fatal("Failed to initialize DB!")
-		}
-	}
+	initConn(connection, true)
+	initDb(ctx, connection.name)
 	initTable()
+
+	// Establish DB connection
+	initConn(connection, false)
+	defer DB.Close()
 
 	// Set server port
 	port := os.Getenv("PORT")
@@ -198,12 +259,11 @@ func main() {
 		log.Printf("No default port set, using 50051...")
 		port = "50051"
 	}
-	var serverPort strings.Builder
-	serverPort.WriteString(":")
-	serverPort.WriteString(port)
 
+	// Start gRPC server
 	log.Printf("Starting GoHome gRPC server on port %s...", port)
-	lis, err := net.Listen("tcp", serverPort.String())
+	serverPort := fmt.Sprintf(":%s", port)
+	lis, err := net.Listen("tcp", serverPort)
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
